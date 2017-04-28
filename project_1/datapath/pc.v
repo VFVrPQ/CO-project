1db392cbc07cf1e598614a4a5b773ab207e4919d
VFVrPQ <15651651997@163.com>
2017-04-28 20:20:23 +0800

    更改initial

--- project_1/datapath/pc.v
+++ project_1/datapath/pc.v
@@ -7,7 +7,7 @@ module pc(NPC,Clk,Reset,PC);
 	always @(posedge Clk)
 	begin
 		if (Reset == 1) 
-			PC <= 30'b00_0000_0000_0000_0011_0000_0000_0000;//true?
+			PC <= 30'b00_0000_0000_0000_0000_1100_0000_0000;//high 30
 		else PC <= NPC;
 	end
 endmodule
